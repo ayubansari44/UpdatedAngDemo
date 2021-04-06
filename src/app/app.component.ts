2f906d79855731c24121bd67679ee72a028b3fd6
Ayub <ayubansari1999@gmail.com>
2021-04-06 11:30:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button'; 
+import { MatSliderModule } from '@angular/material/slider';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-app';
+  formGroup1: FormGroup;
+  formGroup2: FormGroup;
+  formGroup3: FormGroup;
+  formGroup4: FormGroup;
+  formGroup5: FormGroup;
+  inOptional: boolean=false;
+
+  constructor(private _formBuilder: FormBuilder){}
+
+    ngOnInit() {
+      this.formGroup1 = this._formBuilder.group({
+        firstCtlr: ['',Validators.required]
+      });
+
+      this.formGroup2 = this._formBuilder.group({
+        secondCtlr: ['',Validators.required]
+      });   
+      
+      this.formGroup3 = this._formBuilder.group({
+        thirdCtlr: ['',Validators.required]
+      }); 
+
+      this.formGroup4 = this._formBuilder.group({
+        forthCtlr: ['',Validators.required]
+      }); 
+
+      this.formGroup5 = this._formBuilder.group({
+        fiveCtlr: ['',Validators.required]
+      }); 
+    }
+  
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import {MatStepperModule} from '@angular/material/stepper'; 
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import {MatCardModule} from '@angular/material/card'; 
+import { MatTabsModule } from '@angular/material/tabs';
+import { LoginComponent } from './login/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatStepperModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
